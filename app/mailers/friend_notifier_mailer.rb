5ec0b89bd0df19da56816c0846cf76f97892a505
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-11 15:03:41 -0600

    Work following lesson

---
+++ app/mailers/friend_notifier_mailer.rb
@@ -0,0 +1,8 @@
+class FriendNotifierMailer < ApplicationMailer
+  def inform(user, destination_email)
+    @user = user
+    mail(to: destination_email, subject: "#{user.name} says you've changed.")
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+   post '/notification', to: 'notification#create'
    root to: "welcome#index"
    get '/login' => 'sessions#new'
    get '/signup' => 'users#new'
    post '/users' => 'users#create'
    post '/login' => 'sessions#create'
-   get '/logout' => 'sessions#destroy' 
+   get '/logout' => 'sessions#destroy'
 end
